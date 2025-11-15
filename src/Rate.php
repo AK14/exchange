2f14843d98ea96b4535759402826d3fbebfa7dee
AK14 <hooploop14@gmail.com>
2025-11-15 12:28:11 +0300

    init commit

---
+++ src/Rate.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+class Rate
+{
+    public function __construct ()
+    {
+
+    }
+}
---
+++ src/Request/Nbrb.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Request;
+
+class Nbrb
+{
+    protected $currencies;
+    public function __construct()
+    {
+
+    }
+
+    protected function getCurrenciesList()
+    {
+
+
+    }
+
+}
